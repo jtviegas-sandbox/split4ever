f5cfeaf8028887c46c57dd52a57ee68e4e8cd130
jtviegas <jtviegas.js@gmail.com>
2018-06-13 23:59:05 +0100

    closes #39

--- backend/src/resources/parts/route.js
+++ backend/src/resources/parts/route.js
@@ -13,15 +13,10 @@ router.use(bodyParser.json({limit: '50mb'})); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); // for parsing application/x-www-form-urlencoded
 
 router.get('/:id', functions.getPart);
-router.put('/:id', auth.passport.authenticate('basic', { session: false }), functions.setPart);
+router.put('/:id', auth.passport.authenticate('basic', { session: false }), functions.putPart);
 router.get('/', functions.getParts);
+router.post('/', auth.passport.authenticate('basic', { session: false }), functions.postPart);
+router.delete('/:id', auth.passport.authenticate('basic', { session: false }),  functions.delPart);
 
-/*
-
-router.get('/part', functions.getParts);
-
-router.post('/part', functions.setPart);
-router.delete('/part/:id/:rev', functions.delPart);
-*/
 logger.debug('...finished loading.');
 module.exports = router;

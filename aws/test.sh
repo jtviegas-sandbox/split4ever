80be24bd5fe3026d9d7db5db8009dde93ab84553
jtviegas <jtviegas.js@gmail.com>
2018-08-03 01:09:44 +0100

    finished store function and stack implementation

--- aws/test.sh
+++ aws/test.sh
@@ -3,6 +3,5 @@
 this_folder=$(dirname $(readlink -f $0))
 parent_folder=$(dirname $this_folder)
 
-. $this_folder/include
-
-aws lambda delete-function --function-name $FUNCTION_NAME
+export AWS_REGION=us-east-1
+mocha test
--- aws/test/store.js
+++ aws/test/store.js
@@ -1,21 +1,25 @@
 const AWS = require('aws-sdk');
+
 AWS.config.update({ region: process.env.AWS_REGION });
 
 const tester = require('lambda-tester');
 const expect = require('chai').expect;
-const handler = require('../src/index').handler;
+const handler = require('../store/index').handler;
 
 describe('store tests', function(){
     context('parts test', function(){
         it('GET count', function(){
             
             var _event = {
-                table: 'parts'
+                pathParameters:{
+                    storeId: 'parts'
+                } 
                 , httpMethod: 'GET'
             };
             
             return tester(handler).event(_event).expectResult(function(r){ 
-                expect(r.body.Count).to.be.above(0); 
+                let o = JSON.parse(r.body);
+                expect(o.Count).to.be.above(0); 
             });
         });
     });

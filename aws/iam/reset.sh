387b8b7f96b3c36840ff988fb283e35d1e766b26
jtviegas <jtviegas.js@gmail.com>
2018-08-07 23:59:27 +0100

    ended iam part of aws config

--- aws/iam/reset.sh
+++ aws/iam/reset.sh
@@ -8,24 +8,35 @@ _pwd=`pwd`
 
 echo "removing iam configuration for project $PROJ..."
 
-echo "...going to delete store maintenance users..."
+
 for u in $STORE_MAINTENANCE_USERS; do
-    echo "...doing user $u..."
+    aws iam remove-user-from-group --user-name $u --group-name $STORE_MAINTENANCE_GROUP
+    #if [ ! "$?" -eq "0" ] ; then echo "! could not remove user $u from group $STORE_MAINTENANCE_GROUP !...leaving." && cd $_pwd && return 1; else echo "removed user $u from group $STORE_MAINTENANCE_GROUP" ; fi
+    if [ ! "$?" -eq "0" ] ; then echo "! could not remove user $u from group $STORE_MAINTENANCE_GROUP !"; else echo "removed user $u from group $STORE_MAINTENANCE_GROUP" ; fi
     aws iam delete-user --user-name $u
-    if [ ! "$?" -eq "0" ] ; then echo "!!! could not delete user $u!!! ...leaving !!!" ; cd
-    echo "...user $u done..."
+    #if [ ! "$?" -eq "0" ] ; then echo "! could not delete user $u !...leaving." && cd $_pwd && return 1; else echo "deleted user $u" ; fi
+    if [ ! "$?" -eq "0" ] ; then echo "! could not delete user $u !" ; else echo "deleted user $u" ; fi
 done
 
-echo "...going to delete group $STORE_MAINTENANCE_GROUP..."
+
+arn=`aws iam list-policies --output text | grep $STORE_MAINTENANCE_USER_POLICY | awk '{print $2}'`
+aws iam detach-group-policy --group-name $STORE_MAINTENANCE_GROUP --policy-arn $arn
+#if [ ! "$?" -eq "0" ] ; then echo "! could not detach policy $STORE_MAINTENANCE_USER_POLICY from group $STORE_MAINTENANCE_GROUP !...leaving." && cd $_pwd && return 1; else echo "detached policy $STORE_MAINTENANCE_USER_POLICY from group $STORE_MAINTENANCE_GROUP" ; fi
+if [ ! "$?" -eq "0" ] ; then echo "! could not detach policy $STORE_MAINTENANCE_USER_POLICY from group $STORE_MAINTENANCE_GROUP !" ; else echo "detached policy $STORE_MAINTENANCE_USER_POLICY from group $STORE_MAINTENANCE_GROUP" ; fi
 aws iam delete-group --group-name $STORE_MAINTENANCE_GROUP
-if [ ! "$?" -eq "0" ] ; then echo "!!! could not delete group $STORE_MAINTENANCE_GROUP !!! ...leaving !!!" ; cd $_pwd; return -1; fi
-echo "...deleted group $STORE_MAINTENANCE_GROUP..."
+#if [ ! "$?" -eq "0" ] ; then echo "! could not delete group $STORE_MAINTENANCE_GROUP !...leaving." && cd $_pwd && return 1; else echo "deleted group $STORE_MAINTENANCE_GROUP" ; fi
+if [ ! "$?" -eq "0" ] ; then echo "! could not delete group $STORE_MAINTENANCE_GROUP !" ; else echo "deleted group $STORE_MAINTENANCE_GROUP" ; fi
 
-for p in $STORE_MAINTENANCE_USER_POLICY $STORE_MAINTENANCE_FUNCTION_POLICY; do
-    arn=`aws dynamodb describe-table --output text --table-name $t | grep arn.*$t | awk '{print $4}'`
-    arn=`echo $arn  | sed "s/\//\\//g"`
-    store_table_resources=$store_table_resources",\"$arn\""
+for p in $POLICIES; do
+    arn=`aws iam list-policies --output text | grep $p | awk '{print $2}'`
+    for v in `aws iam list-policy-versions --policy-arn $arn --output text | awk '{print $4}'`; do
+        aws iam delete-policy-version --policy-arn $arn --version-id $v
+        #if [ ! "$?" -eq "0" ] ; then echo "! could not delete policy $p version $v !...leaving." && cd $_pwd && return 1; else echo "deleted policy $p version $v" ; fi
+        if [ ! "$?" -eq "0" ] ; then echo "! could not delete policy $p version $v !"; else echo "deleted policy $p version $v" ; fi
+    done
+    aws iam delete-policy --policy-arn $arn
+    #if [ ! "$?" -eq "0" ] ; then echo "! could not delete policy $p !...leaving." && cd $_pwd && return 1; else echo "deleted policy $p" ; fi
+    if [ ! "$?" -eq "0" ] ; then echo "! could not delete policy $p !"; else echo "deleted policy $p" ; fi
 done
-
 
 echo "...iam configuration for project $PROJ was removed."
--- aws/iam/run.sh
+++ aws/iam/run.sh
@@ -8,12 +8,7 @@ _pwd=`pwd`
 
 echo "creating iam configuration for project $PROJ..."
 
-   #"arn:aws:s3:::*/*", "arn:aws:s3:::store_prod.split4ever.com" , "arn:aws:s3:::store_dev.split4ever.com"
-   #sed  "s/.*\"Resource\": \[ XXXYYYZZZ \].*/\"Resource\": \[ abc \]/" storeMaintenanceUser.json
-
 policy=$STORE_MAINTENANCE_USER_POLICY
-echo "...going to create policy $policy..."
-
 store_buckets=""
 for b in $STORE_BUCKET_NAMES; do
     store_buckets=$store_buckets",\"arn:aws:s3:::$b.$DOMAIN\""
@@ -22,19 +17,11 @@ store_buckets_resource="\"arn:aws:s3:::*\/*\""$store_buckets
 
 sed  "s/.*\"Resource\": \[ XXXYYYZZZ \].*/\t\t\"Resource\": \[ $store_buckets_resource \]/" $this_folder/$policy.policy > $this_folder/$policy.json
 aws iam create-policy --policy-name $policy --policy-document file://$this_folder/$policy.json
-if [ ! "$?" -eq "0" ] ; then echo "!!! could not create [policy $policy] !!! ...leaving !!!" ; cd $_pwd; return -1; fi
-echo "...created policy $policy..."
-
-   #"Resource": [  "arn:aws:s3:::store_prod.split4ever.com" , "arn:aws:s3:::store_dev.split4ever.com" ]
-   #"Resource": ["arn:aws:dynamodb:us-east-1:692391178777:table/parts", "arn:aws:dynamodb:us-east-1:692391178777:table/parts"]
-#echo ${v:1:${#v}}
+if [ ! "$?" -eq "0" ] ; then echo "! could not create policy $policy !...leaving." && cd $_pwd && return 1; else echo "created policy $policy" ; fi
 
 policy=$STORE_MAINTENANCE_FUNCTION_POLICY
-echo "...going to create policy $policy..."
 store_buckets_resource=`echo -n $store_buckets | tail -c +2`
-
 sed  "s/.*\"Resource\": \[ XXXXXX \].*/\t\t\"Resource\": \[ $store_buckets_resource \]/" $this_folder/$policy.policy > $this_folder/$policy.2
-
 store_table_resources=""
 for t in $TABLES; do
     arn=`aws dynamodb describe-table --output text --table-name $t | grep arn.*$t | awk '{print $4}'`
@@ -46,31 +33,21 @@ sed  "s=.*\"Resource\": \[ YYYYYY \].*=\t\t\"Resource\": \[ $store_table_resourc
 rm $this_folder/$policy.2
 
 aws iam create-policy --policy-name $policy --policy-document file://$this_folder/$policy.json
-if [ ! "$?" -eq "0" ] ; then echo "!!! could not create [policy $policy] !!! ...leaving !!!" ; cd $_pwd; return -1; fi
-echo "...created policy $policy..."
+if [ ! "$?" -eq "0" ] ; then echo "! could not create policy $policy !...leaving." && cd $_pwd && return 1; else echo "created policy $policy" ; fi
 rm $this_folder/$policy.json
 
-
-echo "...going to create group $STORE_MAINTENANCE_GROUP..."
 aws iam create-group --group-name $STORE_MAINTENANCE_GROUP
-if [ ! "$?" -eq "0" ] ; then echo "!!! could not create [group $STORE_MAINTENANCE_GROUP] !!! ...leaving !!!" ; cd $_pwd; return -1; fi
-echo "...going to attach policy $STORE_MAINTENANCE_USER_POLICY to group $STORE_MAINTENANCE_GROUP..."
+if [ ! "$?" -eq "0" ] ; then echo "! could not create group $STORE_MAINTENANCE_GROUP !...leaving." && cd $_pwd && return 1; else echo "created group $STORE_MAINTENANCE_GROUP" ; fi
 arn=`aws iam list-policies --output text | grep $STORE_MAINTENANCE_USER_POLICY | awk '{print $2}'`
 aws iam attach-group-policy --policy-arn $arn --group-name $STORE_MAINTENANCE_GROUP
-if [ ! "$?" -eq "0" ] ; then echo "!!! could not attach policy $STORE_MAINTENANCE_USER_POLICY to group $STORE_MAINTENANCE_GROUP !!! ...leaving !!!" ; cd $_pwd; return -1; fi
-echo "...created group $STORE_MAINTENANCE_GROUP..."
+if [ ! "$?" -eq "0" ] ; then echo "! could not attach policy $STORE_MAINTENANCE_USER_POLICY to group $STORE_MAINTENANCE_GROUP !...leaving." && cd $_pwd && return 1; else echo "attached policy $STORE_MAINTENANCE_USER_POLICY to group $STORE_MAINTENANCE_GROUP" ; fi
 
-echo "...going to create store maintenance users and add them to the group $STORE_MAINTENANCE_GROUP..."
 for u in $STORE_MAINTENANCE_USERS; do
-    echo "...doing user $u..."
     aws iam create-user --user-name $u
-    if [ ! "$?" -eq "0" ] ; then echo "!!! could not create user $u!!! ...leaving !!!" ; cd
+    if [ ! "$?" -eq "0" ] ; then echo "! could not create user $u !...leaving." && cd $_pwd && return 1; else echo "created user $u" ; fi
     aws iam add-user-to-group --user-name $u --group-name $STORE_MAINTENANCE_GROUP
-    if [ ! "$?" -eq "0" ] ; then echo "!!! could not add user $u to group $STORE_MAINTENANCE_GROUP !!! ...leaving !!!" ; cd
-    echo "...user $u done..."
+    if [ ! "$?" -eq "0" ] ; then echo "! could not add user $u to group $STORE_MAINTENANCE_GROUP !...leaving." && cd $_pwd && return 1; else echo "added user $u to group $STORE_MAINTENANCE_GROUP" ; fi
 done
-
-
 
 echo "... iam configuration for project $PROJ done."
 
